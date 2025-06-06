fb76b880bcf879f02a6669e65228cf6c5f05d8a0
Isha Shewale <shewaleisha@gmail.com>
2025-06-06 13:01:07 +0530

    Initial commit

---
+++ RoshDip/smsapi.php
@@ -0,0 +1,13 @@
+<?php
+$smsstatus="Disabled";
+$smsapi = "sdfjakjfjsldfhsdkfhgi";
+$senderid = "RoshDip";
+/*
+customer purchase request
+farmer approval for request
+customer payment notification
+
+worker hire
+worker job
+*/
+?>
